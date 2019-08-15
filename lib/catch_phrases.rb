426781be15df1699e6b65825e9cc96edfc90935d
Renard Burnett <renxburnett@gmail.com>
2019-08-15 20:23:12 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,28 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
+def mario()
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-def toadstool
+def toadstool()
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+end
+
+def link()
+  status = "It's Dangerous To Go Alone! Take This."
+  puts status
+end
+
+def any_phrase(input)
+  phrase = "Do A Barrel Roll!"
+
+  if input == ''
+    puts phrase
+  elsif input == ""
+    puts phrase
+  elsif input == nil
+    puts phrase
+  else
+    puts input
 end
